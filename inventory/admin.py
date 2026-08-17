53d216a911e18fe8c1287ae6702ab9be51810646
Nolaj <davidjalon@protonmail.com>
2026-08-17 17:22:31 +0200

    new: migration new bd

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Material
 
 # Register your models here.
+@admin.register(Material) # Decorador que registra el modelo en el admin
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "marca", "tipo", "color", "diametro", "costo_por_kg", "stock_minimo") # Columnas
+    list_filter = ["tipo"] # Filtros
+    search_fields = ("nombre", "marca") # Caja de búsqueda
