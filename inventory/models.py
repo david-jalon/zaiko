948ad83f7d0d4d9e744919166d89c098fba6bfd3
Nolaj <davidjalon@protonmail.com>
2026-08-17 16:44:02 +0200

    add inventory models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+TIPO_CHOICES = [("PLA", "PLA"), ("PETG", "PETG"), ("ABS", "ABS"), ("TPU", "TPU")]
+
+
 # Create your models here.
+class Material(models.Model):
+    nombre = models.CharField(max_length=100)
+    marca = models.CharField(max_length=30)
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES)
+    color = models.CharField(max_length=30)
+    diametro = models.FloatField()
+    costo_por_kg = models.DecimalField(max_digits=4, decimal_places=2)
+    stock_minimo = models.FloatField()
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ["nombre"]
